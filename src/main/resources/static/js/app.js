ddd55979f2a310491ad1ae5bfbefb84250baf5c7
Vitor Henrique <vitorhenriquec@ufrn.edu.br>
2019-03-19 22:37:20 -0300

    Finalizando a adição de produtos

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,12 +4,24 @@ appOrderSystem.controller("indexController", function($scope,$http){
 	$scope.nome = "Vitor";
 	$scope.produtos = []
 	$scope.produto = {}
+	
+	$scope.carregarInicio = function(){
+		$http({
+			  method: 'GET',
+			  url: 'http://localhost:8080'
+			}).then(function successCallback(response) {
+				console.log(response.status);
+			  }, function errorCallback(response) {
+				  console.log(response.status);
+			  });
+	};
+	
 	$scope.carregarProdutos = function(){
 		$http({
 			  method: 'GET',
 			  url: 'http://localhost:8080/produto'
 			}).then(function successCallback(response) {
-				$scope.produtos = response.data
+				$scope.produtos = response.data;
 			  }, function errorCallback(response) {
 				  console.log(response.status);
 			  });
@@ -20,11 +32,10 @@ appOrderSystem.controller("indexController", function($scope,$http){
 	
 	$scope.salvarProduto = function(){
 		$http({
-			  method: 'POST',
-			  url: 'http://localhost:8080/produto',
-			  data: produto
+			  method: 'POST', url: 'http://localhost:8080/produto', data: $scope.produto
 			}).then(function successCallback(response) {
 				console.log(response.status);
+				$scope.carregarInicio();
 			  }, function errorCallback(response) {
 				  console.log(response.status);
 			  });
