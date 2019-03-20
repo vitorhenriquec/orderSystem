c74793f97bf38ffd068a32d5dcf21a9a2a296c08
Vitor Henrique <vitorhenriquec@ufrn.edu.br>
2019-03-20 00:39:07 -0300

    Adicionando arquivos para as páginas e a função de alteração de produto.

--- src/main/java/com/ordersystems/controller/HomeController.java
+++ src/main/java/com/ordersystems/controller/HomeController.java
@@ -7,7 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String home() {
+	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/produto")
+	public String produto() {
+		return "produto";
+	}
+	
+	@RequestMapping("/cardapio")
+	public String cardapio() {
+		return "cardapio";
+	}
+	
+	@RequestMapping("/pedido")
+	public String pedido() {
+		return "pedido";
+	}
+	
+	@RequestMapping("/mesa")
+	public String mesa() {
+		return "produto";
 	}
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,7 +4,7 @@ appOrderSystem.controller("indexController", function($scope,$http){
 	$scope.produtos = []
 	$scope.produto = {}
 		
-	$scope.carregarProdutos = function(){
+	carregarProdutos = function(){
 		$http({
 			  method: 'GET',
 			  url: 'http://localhost:8080/produto'
@@ -16,14 +16,11 @@ appOrderSystem.controller("indexController", function($scope,$http){
 			  });
 	};
 	
-	
-	$scope.carregarProdutos();
-	
 	$scope.salvarProduto = function(){
 		$http({
 			  method: 'POST', url: 'http://localhost:8080/produto', data: $scope.produto
 			}).then(function successCallback(response) {
-				console.log(response.status);
+				carregarProdutos();
 			  }, function errorCallback(response) {
 				  console.log(response.status);
 			  });
@@ -34,27 +31,25 @@ appOrderSystem.controller("indexController", function($scope,$http){
 			  method: 'DELETE', url: 'http://localhost:8080/produto/'+id, 
 			}).then(function successCallback(response) {
 				for(var i = 0; i < $scope.produtos.length; i++){
-					if($scope.produtos[i].id == id){
+					if($scope.produtos[i].id == parseFloat(id)){
 						$scope.produtos.slice(i,1);
+						carregarProdutos();
 						break;
 					}
 				}
-				console.log(response.status);
 			  }, function errorCallback(response) {
 				  console.log(response.status);
 			  });
 	};
 	
-	$scope.alterarProduto = function(){
-		$http({
-			  method: 'PUT', url: 'http://localhost:8080/produto', data: $scope.produto
-			}).then(function successCallback(response) {
-				$scope.produtos.push.apply($scope.produtos,response.data);
-				console.log(response.status);
-			  }, function errorCallback(response) {
-				  console.log(response.status);
-			  });
+	$scope.alterarProduto = function(pro){
+		$scope.produto = angular.copy(pro);
 	};
+	
+	$scope.cancelar = function(){
+		$scope.produto = {};
+	}; 
+	carregarProdutos();
 });
 
 appOrderSystem.controller("pedidoController", function(){
