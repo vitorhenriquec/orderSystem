8239f382e0414542d698b6d72b62f57c7699c687
Vitor Henrique <vitorhenriquec@ufrn.edu.br>
2019-04-06 10:34:01 -0300

    Corrigindo o mapeamento

--- src/main/java/com/ordersystems/controller/PedidoController.java
+++ src/main/java/com/ordersystems/controller/PedidoController.java
@@ -11,12 +11,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ordersystems.domain.Pedido;
+import com.ordersystems.service.CardapioService;
 import com.ordersystems.service.PedidoService;
+import com.ordersystems.service.ProdutoService;
 
 @RestController
 public class PedidoController {
 	@Autowired
 	PedidoService pedidoService;
+	
+	@Autowired 
+	ProdutoService produtoService;
+	
+	@Autowired
+	CardapioService cardapioService;
 	
 	@RequestMapping(method=RequestMethod.GET,value="/pedido",produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> buscarTodos(){
@@ -24,19 +32,19 @@ public class PedidoController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST,value="/pedido",consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<?> adicionarProduto(@RequestBody Pedido pedido){
+	public ResponseEntity<?> adicionarPedido(@RequestBody Pedido pedido){
 		pedidoService.adicionar(pedido);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT,value="/pedido",produces = MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<?> alterarProduto(@RequestBody Pedido pedido){
+	public ResponseEntity<?> alterarPedido(@RequestBody Pedido pedido){
 		pedidoService.salvar(pedido);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE,value="/pedido/{id}")
-	public ResponseEntity<?> removerProduto(@PathVariable Integer id){
+	public ResponseEntity<?> removerPedido(@PathVariable Integer id){
 		Pedido pedidoEncontrado = pedidoService.buscarPorId(id).get();
 		if(pedidoEncontrado == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/com/ordersystems/domain/Pedido.java
+++ src/main/java/com/ordersystems/domain/Pedido.java
@@ -6,13 +6,12 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,8 +24,10 @@ public class Pedido implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
-	@OneToMany(cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY, mappedBy = "pedido")
+	@ManyToMany(cascade = {
+			CascadeType.PERSIST,
+            CascadeType.MERGE
+        },mappedBy = "pedidos")
 	private List<Produto> produtos = new ArrayList<Produto>();
 	
 	@ManyToOne
