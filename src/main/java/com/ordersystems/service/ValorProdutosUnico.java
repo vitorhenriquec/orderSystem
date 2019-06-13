1fbe88355bf6a67cefc603671c3357d746f780c7
Vitor Henrique <vitorhenrique908@gmail.com>
2019-06-13 01:00:17 -0300

    Adicionando ponto flexível da promoção para o lava-jato

--- src/main/java/com/ordersystems/service/ValorProdutosUnico.java
+++ src/main/java/com/ordersystems/service/ValorProdutosUnico.java
@@ -3,17 +3,17 @@ package com.ordersystems.service;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegraPromocaoProduto implements RegraPromocao{
+public class ValorProdutosUnico implements RegraPromocao{
 
 	@Override
 	public double calcularDesconto(double valorPedido) {
-		return(double) (valorPedido - (valorPedido*0.05));
+		return(double) (valorPedido - (valorPedido*0.04));
 	}
 
 	@Override
 	public Map<String, String> gerarInformacao(double valorPedido) {
 		Map<String, String> dict = new HashMap<String, String>();
-		String mensagem= "Promoção dos 5%: A quantidade produtos comprados foi maior do que 2 e o valor da compra teve 5% de desconto";
+		String mensagem= "Promoção dos 4%: A sua compra tem mais do que produtos diferentes que totalizam mais do que $100.00 e o valor da compra teve 4% de desconto";
 		String valorTotal = String.valueOf(valorPedido);
 		String valorFinal = String.valueOf(this.calcularDesconto(valorPedido));
 		dict.put("mensagem", mensagem);
