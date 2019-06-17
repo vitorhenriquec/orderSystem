60fffa61526c5f98344ab0d9397a36b2f2f1e5cd
GildoAugu <gildo.augu@gmail.com>
2019-06-17 18:31:18 -0300

    Commitando uma mudança no ProdutoRepository

--- src/main/java/com/ordersystems/repository/ProdutoRepository.java
+++ src/main/java/com/ordersystems/repository/ProdutoRepository.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Repository;
-
 import com.ordersystems.domain.Produto;
 
 @NoRepositoryBean
