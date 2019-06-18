a797fe98144267335ce1d277374564f5210a4999
obama <obama@imd.ufrn.br>
2019-06-18 12:21:49 -0300

    Mudando a regra

--- src/main/java/com/ordersystems/service/PedidoService.java
+++ src/main/java/com/ordersystems/service/PedidoService.java
@@ -61,11 +61,11 @@ public class PedidoService {
 		
 		}
 		
-		if(valorPedido > 50.0) {
+		if(valorPedido >= 100.0) {
 			regra = new RegraPromocaoValor();
 			criarInformacao = regra.gerarInformacao(valorPedido);
 		}
-		else if(quantProdutos >= 3) {
+		else if(quantProdutos > 4) {
 			regra = new RegraPromocaoQuantidade();
 			criarInformacao = regra.gerarInformacao(valorPedido);
 		}
